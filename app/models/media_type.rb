a95adedfd5e13daf71cd0a66957d2c4729b7661b
OUAFIR <sofiane.ouafir@live.fr>
2017-07-20 19:51:27 +0200

    In this version I have: - Added some validations on MediaType - added a fixture for MediaType - written some model test on media_type_test

--- app/models/media_type.rb
+++ app/models/media_type.rb
@@ -1,3 +1,6 @@
 class MediaType < ApplicationRecord
+    before_validation {self.name.strip!}
+    before_save { self.name.downcase! }
+    validates :name, presence: true, length: {minimum:2, maximum: 50}
     has_many :supports
 end
--- test/models/media_type_test.rb
+++ test/models/media_type_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class MediaTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    def setup
+        @media_type = MediaType.new(name:"book")
+    end
+
+    test "should be valid" do
+        assert @media_type.valid?
+    end
+
+    test "name should never be blank" do
+        @media_type.name = "   "
+        assert_not @media_type.valid?
+    end
+
+    test "name should at least have 2 characters" do
+        @media_type.name = "a"
+        assert_not @media_type.valid?
+    end
+
+    test "name should at most have 50 characters" do
+        @media_type.name = "a" * 51
+        assert_not @media_type.valid?
+    end
+
+    test "name should be trimmed before validation" do
+        name = " music "
+        @media_type.name = name
+        @media_type.valid?
+        assert_equal name.strip, @media_type.name
+    end
+
+    test "name should be lowercased before save" do
+        name = "Course"
+        @media_type.name = name
+        @media_type.save
+        assert_equal name.downcase, @media_type.name
+    end
 end
