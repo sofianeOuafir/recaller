9143fd3874e6ef746eaaf2005939215fe6de4196
OUAFIR <sofiane.ouafir@live.fr>
2017-07-31 19:35:28 +0200

    ...

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,5 @@
 class Language < ApplicationRecord
+    #TODO language name need to be capitalized before save
     before_validation {self.name.strip!; self.code.strip!}
     validates :name, presence: true, length: {minimum:2, maximum:50}
     validates :code, presence: true, length: {is: 2}
