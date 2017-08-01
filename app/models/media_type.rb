abcd5c82359dc96237a42a3751a6a3440b6bc9b5
OUAFIR <sofiane.ouafir@live.fr>
2017-08-01 23:04:40 +0200

    UI: work in progress

--- app/models/media_type.rb
+++ app/models/media_type.rb
@@ -1,4 +1,5 @@
 class MediaType < ApplicationRecord
+    #TODO mediaType name need to be capitalized before save
     before_validation {self.name.strip!}
     before_save { self.name.downcase! }
     validates :name, presence: true, length: {minimum:2, maximum: 50}
