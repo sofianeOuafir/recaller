bff05c329ce0031c4e1481715a787587dddcc024
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2017-08-26 00:10:31 +0200

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'sofiane.chronicle@gmail.com',
-    password:             '4c6t3xxx',
+    password:             '',
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
